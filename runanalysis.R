c934e80fc5fced40ed814f100d9275b17ae99561
xaviergm <xavier.gutierrez@roche.com>
2015-05-14 22:52:56 +0200

    commentary added to script

--- runanalysis.R
+++ runanalysis.R
@@ -11,7 +11,8 @@ if (!file.exists("./GaCD")) {
 setwd("./GaCD")
 
 ## Download the required file
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+fileUrl <- paste("https://d396qusza40orc.cloudfront.net/",
+        "getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",sep="")
 download.file(fileUrl,"./UCI HAR Dataset.zip",method="internal",mode="wb")
 
 ## Unzip the required file
@@ -27,7 +28,8 @@ subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 features <- read.table("./UCI HAR Dataset/features.txt")
 
-## Merge the training and test data sets into one
+## Merge the training and test data sets into one. This is done by row-binding 
+## the training and test data sets together.
 X_all <- rbind(X_test,X_train)
 y_all <- rbind(y_test,y_train)
 Subject <- rbind(subject_test,subject_train)
@@ -41,14 +43,16 @@ for (i in 1:length(y_all[,1])) {
 ## Use the features vector as the names of the columns in the data set.
 names(X_all) <- t(features)[2,]
 ## Take only those columns with means or standard deviations
-std_mean <- sort(c(unlist(grep("std()",names(X_all))),unlist(grep("mean()",names(X_all)))))
+std_mean <- sort(c(unlist(grep("std()",names(X_all))),unlist(grep("mean()",
+        names(X_all)))))
 X_std_mean <- X_all[,std_mean]
 
 ## Bind Activity, Subject and the data frame with means and stds only.
 complete_all <- cbind(Activity,Subject,X_std_mean)
 names(complete_all)[2]<-"Subject"
 
-## Now perform  the required averages
+## Now perform  the required averages using the dplyr package group_by() and
+## summarise_each() functions.
 complete_grouped <- group_by(complete_all,Subject,Activity)
 complete_summarized <- summarise_each (complete_grouped,funs(mean))
 
